0a92a1ef8cfb094a84073bf841ffbc54a31b108b
hojas <cyxhuan9@163.com>
2023-02-10 16:16:13 +0800

    remove vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
@@ -12,17 +11,11 @@ export default defineConfig({
     rollupOptions: {
       external: ['vue'],
       output: {
-        sourcemap: false,
         globals: {
           vue: 'Vue',
         },
       },
     },
   },
-  plugins: [
-    vue(),
-    dts({
-      include: 'src',
-    }),
-  ],
+  plugins: [vue()],
 })
